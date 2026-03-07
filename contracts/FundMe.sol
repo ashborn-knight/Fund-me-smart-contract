4dd2e2fc93e2fcd439b77aa7eae8e4f666ab8b03
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-07 22:45:42 +0000

    adding more functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     uint256  public minimumUsd = 5;
@@ -19,9 +20,20 @@ contract FundMe {
     function getPrice() public{
     //Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
     //ABI
+    //Aggregaterv3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version;
+    AggregatorV3Interface priceFeed = Aggregatorv3Interface();
+    priceFeed.latestRoundData();
     }
      
   //  function withdraw() public{}
+
+  function getConversionRate() public {
+
+  }
+
+  function getVersion() public returns(uint256) {
+    return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+  }
 
 
 }
