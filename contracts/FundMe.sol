ad1751cd36721865eca22bac111048b97ba4ee86
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-07 04:18:55 +0000

    initial commit with FundMe contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    uint256  public minimumUsd = 5;
+
+    function fund() public payable {
+     // Allow users to send $
+     // Have a minimum $ sent
+     // How to send ETH to this contract
+     require(msg.value >= minimumUsd,"didnt send enough ETH"); //1e18 = 1 ETH = 1 * 10 ** 18
+
+    //A revert is undoing any action that has been done and sending the remaining gas back
+    // a transaction spends gas....whether its a failure or not
+
+    }
+     
+  //  function withdraw() public{}
+
+
+}
